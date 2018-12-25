6b5655cf2589bdb4eec70cc81022f7943a3e777e
riverrrflow <riverrrflow@gmail.com>
2018-12-25 22:38:16 +0800

    test

---
+++ tomcat-learning/src/test/java/com/river/test/SimpleTest.java
@@ -0,0 +1,17 @@
+package com.river.test;
+
+import org.junit.Test;
+
+/**
+ * 测试类
+ *
+ * @author beihong
+ * @create 2018-12-25 10:35 PM
+ */
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        System.out.println("TEST");
+    }
+}
